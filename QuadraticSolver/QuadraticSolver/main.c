88e1a9daa56062b54f525aa3a78071681e32475e
Alex Hill <alexhill.c@gmail.com>
2016-02-08 10:45:48 -0500

    -White space commit, remove extraneous files

--- QuadraticSolver/QuadraticSolver/main.c
+++ QuadraticSolver/QuadraticSolver/main.c
@@ -9,29 +9,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <complex.h>
 
 int main(int argc, const char * argv[]) {
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
     int c = atoi(argv[3]);
-    
+
     printf("%d, %d, %d\n", a, b, c);
-    
+
     float rootArg = (b * b - (4 * a * c));
     if (rootArg > 0.0) {
         //then this has two real roots
-        float posRoot = (-b + sqrt(rootArg))/(2 * a);
-        float negRoot = (-b - sqrt(rootArg))/(2 * a);
+        float posRoot = (-b + sqrt(rootArg)) / (2 * a);
+        float negRoot = (-b - sqrt(rootArg)) / (2 * a);
         printf("The positive root is: %f and negative root is: %f \n", posRoot, negRoot);
-    }else if (rootArg == 0.0){
+    } else if (rootArg == 0.0) {
         //there is a singular root
         float root = -b/(2*a);
         printf("The root is: %f\n",root);
-    }else{
+    } else {
         //there is no real root
-        float Real = -b/(2*a);
-        float Img = sqrt(-rootArg);
-        printf("The positive imaginary root is: %f+%fi and the negative imaginary root is: %f-%fi \n",Real,Img,Real,Img);
+        float real = -b/(2*a);
+        float img = sqrt(-rootArg);
+        printf("The positive imaginary root is: %f+%fi and the negative imaginary root is: %f-%fi \n", real, img, real, img);
     }
 }
